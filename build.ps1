0d245a47df1a9a0ee06f0cce3f455d56b33dd7e7
Matt Wanchap <matt.wanchap@cpal.com.au>
2018-04-02 11:19:10 +1000

    Initial commit

---
+++ build.ps1
@@ -0,0 +1,12 @@
+﻿$websiteDir = "C:\Code\mwanchap.github.io"
+set-location $PSScriptRoot
+
+# build the output
+.\graze.exe
+
+# move the generated output into the site's github dir
+copy-item -Path ".\output\index.html" -Destination "$websiteDir\index.html"
+copy-item -Path ".\output\assets\" -Destination $websiteDir -Recurse -Force
+
+write-host "Copied output into website dir"
+write-host "Now cd over to $websiteDir, commit and push"
